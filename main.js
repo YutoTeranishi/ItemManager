388d641b7a84c24200bfb9e31cfbecde2f92f6ba
YutoTeranishi <96603517+YutoTeranishi@users.noreply.github.com>
2021-12-24 17:30:41 +0900

    from raspberrypi

---
+++ main.js
@@ -0,0 +1,19 @@
+//DOMの読み込み終了時の処理を登録
+window.addEventListener('DOMContentLoaded',function(){
+  //"id=calc"のボタンを登録
+  let elCalc = document.querySelector('#calc');
+
+  elCalc.addEventListener('click',function(){
+    let prices = document.querySelectorAll('.price');
+
+    let priceSum = 0;
+    for(let i = 0; i < prices.length; i++){
+      let priceVal = prices[i].value;
+      priceSum = priceSum + parseInt(priceVal);
+    }
+
+    let html = '合計金額:<strong>' + priceSum + '</strong>円</div>';
+
+    document.querySelector('#output').innerHTML = html;
+  });
+});
