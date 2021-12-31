680269e563d97a99f04cc86619a8642a53bfd8ad
Yuto Teranishi <yt05271992@yahoo.co.jp>
2021-12-31 18:41:09 +0900

    購入手数料の項目数の取得を追加

--- pdo_itemManagerDB.php
+++ pdo_itemManagerDB.php
@@ -28,6 +28,7 @@
         $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         //echo "データベース{$dbName}に接続しました。";
 
+        //仕入れ、販売手数料のテーブルを取得
         $sql_pSet = "SELECT service_name,percentage_of_fee FROM pSetUp_fee_table";
         //プリペアドステートメント作成
         $stm_pSet = $pdo->prepare($sql_pSet);
@@ -36,6 +37,9 @@
 
         //結果の取得(連想配列)
         $result_pSet = $stm_pSet->fetchAll(PDO::FETCH_ASSOC);
+        //登録されているサービス数を取得
+        $result_pSet_count = count($result_pSet);
+        //print($result_pSet_count);
 
         $sql = "SELECT * FROM price_table";
         //プリペアドステートメント作成
@@ -50,31 +54,38 @@
         echo '<table border="1" id="priceTable">';
         echo "<thead><tr>";
         echo "<th>", "ID","</th>";
-        echo "<th>", "商品ID","</th>";
+        echo "<th>", "商品<br>ID","</th>";
         echo "<th>", "購入回","</th>";
         echo "<th>","単価(元)","</th>";
         echo "<th>","変換<br>レート","</th>";
         echo "<th>","単価(円)","</th>";
-        echo "<th>","購入個数","</th>";
+        echo "<th>","購入<br>個数","</th>";
         echo "<th>","同時<br>発送個数","</th>";
         echo "<th>","中国<br>国内送料","</th>";
-        echo "<th>","国際送料","</th>";
+        echo "<th>","国際<br>送料","</th>";
+        echo "<th>","一個あたりの<br>国際送料","</th>";
+        echo "<th>","買付手数料","</th>";
         //echo "<th>","購入手数料","</th>";
         /*
         foreach ($result_pSet as $p_sevice_name) {
               echo "<th>",$p_sevice_name['service_name'],"</th>";
         }*/
-        echo "<th>","仕入れ原価","</th>";
+        echo "<th>","仕入れ<br>原価","</th>";
         echo "<th>","販売時<br>予定送料","</th>";
-        echo "<th>","販売予定価格","</th>";
+        echo "<th>","販売<br>予定価格","</th>";
         foreach ($result_pSet as $p_sevice_name) {
-              echo "<th>",$p_sevice_name['service_name'],"</th>";
+              echo "<th>",$p_sevice_name['service_name'],"<br>","手数料(",$p_sevice_name['percentage_of_fee'],"%)</th>";
         }
 
-        echo "<th>","単体利益額","</th>";
-        echo "<th>","単体利益額","</th>";
-        echo "<th>","合計利益額","</th>";
-        echo "<th>","合計利益額","</th>";
+        foreach ($result_pSet as $p_sevice_name) {
+          echo "<th>",$p_sevice_name['service_name'],"<br>単体利益額","</th>";
+        }
+
+        foreach ($result_pSet as $p_sevice_name) {
+          echo "<th>",$p_sevice_name['service_name'],"<br>総利益額","</th>";
+        }
+        //echo "<th>","合計<br>利益額","</th>";
+        //echo "<th>","合計<br>利益額","</th>";
 
         echo "</tr></thead>";
 
@@ -82,6 +93,7 @@
         echo "<tbody>";
         // カウンター
         $i=0;
+        $price_yen = 0;
         //resultをrowとして各行処理
         foreach($result as $row){
           echo "<tr>";
@@ -90,11 +102,16 @@
           echo "<td>",$row['times_id'],"</td>";
           echo "<td>",$row['price_gen'],"</td>";
           echo "<td>",$row['rate_genToyen'],"</td>";
-          echo "<td>","300","</td>";
+          //単価(円)を計算
+          $price_yen = $row['price_gen']*$row['rate_genToyen'];
+          echo "<td>",$price_yen,"</td>";
+
           echo "<td>",$row['amount_Item'],"</td>";
           echo "<td>",$row['total_amount_Items'],"</td>";
           echo "<td>",$row['trans_fee_in_China'],"</td>";
           echo "<td>","6500","</td>";
+          echo "<td>","200","</td>";
+          echo "<td>","100","</td>";
           echo "<td>","350","</td>";
           //echo "<td>",$row['trans_fee_in_Japan'],"</td>";
           echo '<td><input type="number" value="',$row['trans_fee_in_Japan'],'" class="price" style="width:70px;"></td>';
@@ -116,16 +133,17 @@
           foreach ($result_pSet as $pecentage_of_pfee) {
             echo "<td>",$pecentage_of_pfee['percentage_of_fee'],"%</td>";
           }
-
-          echo "<td>","testpriceA","</td>";
+          
+          echo "<td id=uniprice_",$i,">","testpriceA","</td>";
           echo "<td>","testpriceB","</td>";
-          echo "<td>","testpriceC","</td>";
+          echo "<td id=totalprice_",$i,">","testpriceC","</td>";
           echo "<td>","testpriceD","</td>";
 
           //ボタンのidを行数に応じて設定
           echo '<td><button id="calc_',$i,'">';
           echo '損益を計算</button></td>';
           echo "</tr>";
+          $price_yen = 0;
           $i++;
         }
         echo "</tbody>";
